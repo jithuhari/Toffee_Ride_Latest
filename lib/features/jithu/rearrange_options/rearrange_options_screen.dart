11160d534aed65b1ea6805af7bb2c75d7a28a30f
sirin <sibiin@columnit.com>
2022-01-03 11:45:02 +0530

    remove overflow and align the list to center

--- lib/features/jithu/rearrange_options/rearrange_options_screen.dart
+++ lib/features/jithu/rearrange_options/rearrange_options_screen.dart
@@ -38,98 +38,127 @@ class _RearrangeScreenState extends State<RearrangeScreen> {
                     children: [
                       Expanded(
                         flex: 8,
-                        child: Row(
-                          //crossAxisAlignment: CrossAxisAlignment.center,
+                        child: Column(
                           children: [
-                            Expanded(
-                              flex: 3,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Container(
-                                  height:
-                                      MediaQuery.of(context).size.height * .83,
-                                  decoration:
-                                      BoxDecoration(color: Colors.lightBlue),
-                                  child: ListView(
-                                    padding: EdgeInsets.all(10),
-                                    //shrinkWrap: true,
-                                    //mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                    children: [
-                                      TextCard(
-                                        text: 'Let Us',
-                                      ),
-                                      TextCard(
-                                        text: 'I am',
-                                      ),
-                                      TextCard(
-                                        text: 'Do not',
-                                      ),
-                                      TextCard(
-                                        text: 'We have',
-                                      ),
-                                      TextCard(
-                                        text: 'She is',
-                                      ),
-                                      TextCard(
-                                        text: 'They',
-                                      ),
-                                    ],
-                                  ),
-                                ),
+                            Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Text(
+                                'Rearrange the options on the right to match with the items in the left',
+                                style: TextStyle(
+                                    color: Colors.blue.shade900,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20),
                               ),
                             ),
-                            Expanded(
-                              flex: 2,
-                              child: Center(
-                                child: Container(
-                                  height:
-                                      MediaQuery.of(context).size.height * .83,
-                                  decoration:
-                                      BoxDecoration(color: Colors.lightBlue),
-                                  child: ReorderableListView(
-                                    //buildDefaultDragHandles: false,
-                                    padding: EdgeInsets.all(15),
-                                    //shrinkWrap: true,
-                                    onReorder: (oldIndex, newIndex) {
-                                      controller.reorder(oldIndex, newIndex);
-                                    },
-                                    children: controller.myFav
-                                        .map(
-                                          (e) => Container(
-                                            key: ValueKey(e),
-                                            height: 50,
-                                            width: MediaQuery.of(context)
-                                                    .size
-                                                    .width *
-                                                .15,
-                                            decoration: BoxDecoration(
-                                                color: Colors.blueAccent,
-                                                borderRadius:
-                                                    BorderRadius.circular(10),
-                                                boxShadow: [
-                                                  BoxShadow(
-                                                    color: Colors.grey,
-                                                    offset: Offset(
-                                                        0.0, 1.0), //(x,y)
-                                                    blurRadius: 6.0,
-                                                  ),
-                                                ]),
-                                            child: Center(
-                                              child: Text(
-                                                e,
-                                                style: TextStyle(
-                                                  fontSize: 20,
-                                                  fontWeight: FontWeight.bold,
-                                                  color: Colors.white,
-                                                ),
-                                              ),
-                                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * .05,
+                            ),
+                            Row(
+                              //crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Expanded(
+                                  flex: 2,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              .7,
+                                      decoration: BoxDecoration(
+                                          color: Colors.lightBlue,
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: ListView(
+                                        padding: EdgeInsets.all(16),
+                                        //shrinkWrap: true,
+                                        //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                        children: [
+                                          TextCard(
+                                            text: 'Let Us',
                                           ),
-                                        )
-                                        .toList(),
+                                          TextCard(
+                                            text: 'I am',
+                                          ),
+                                          TextCard(
+                                            text: 'Do not',
+                                          ),
+                                          TextCard(
+                                            text: 'We have',
+                                          ),
+                                          TextCard(
+                                            text: 'She is',
+                                          ),
+                                          TextCard(
+                                            text: 'They',
+                                          ),
+                                        ],
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
+                                Expanded(
+                                  flex: 2,
+                                  child: Center(
+                                    child: Container(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              .7,
+                                      decoration: BoxDecoration(
+                                          color: Colors.lightBlue,
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: ReorderableListView(
+                                        //buildDefaultDragHandles: false,
+                                        padding: EdgeInsets.all(16),
+                                        //shrinkWrap: true,
+                                        onReorder: (oldIndex, newIndex) {
+                                          controller.reorder(
+                                              oldIndex, newIndex);
+                                        },
+                                        children: controller.myFav
+                                            .map(
+                                              (e) => Container(
+                                                key: ValueKey(e),
+                                                height: MediaQuery.of(context)
+                                                        .size
+                                                        .height *
+                                                    .1,
+                                                width: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    .15,
+                                                decoration: BoxDecoration(
+                                                    color: Colors.blueAccent,
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10),
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                        color: Colors.grey,
+                                                        offset: Offset(
+                                                            0.0, 1.0), //(x,y)
+                                                        blurRadius: 6.0,
+                                                      ),
+                                                    ]),
+                                                child: Center(
+                                                  child: Text(
+                                                    e,
+                                                    style: TextStyle(
+                                                      fontSize: 20,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      color: Colors.white,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            )
+                                            .toList(),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
@@ -151,14 +180,6 @@ class _RearrangeScreenState extends State<RearrangeScreen> {
                               ),
                               Column(
                                 children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Image(
-                                        width: 50,
-                                        height: 50,
-                                        image: AssetImage(
-                                            'assets/images/newicons/Repeat_150.png')),
-                                  ),
                                   Padding(
                                     padding: const EdgeInsets.all(8.0),
                                     child: Image(
@@ -204,7 +225,7 @@ class TextCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
+      height: MediaQuery.of(context).size.height * .1,
       width: MediaQuery.of(context).size.width * .15,
       decoration: BoxDecoration(
           color: Colors.blueAccent,
